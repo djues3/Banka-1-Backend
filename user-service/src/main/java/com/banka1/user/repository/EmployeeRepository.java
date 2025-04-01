6e90a36abf27d252cc53c447da09b5c8c67b648d
David Djuretanovic <djues3@gmail.com>
2025-04-01 07:21:33 +0200

    Merged customer and employee tables
    This makes the IdP work simpler
    
    Signed-off-by: David Djuretanovic <djues3@gmail.com>

--- user-service/src/main/java/com/banka1/user/repository/EmployeeRepository.java
+++ user-service/src/main/java/com/banka1/user/repository/EmployeeRepository.java
@@ -2,22 +2,16 @@ package com.banka1.user.repository;
 
 import com.banka1.common.model.Department;
 import com.banka1.user.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends UserRepository<Employee> {
 
-    Optional<Employee> findByEmail(String email);
     List<Employee> findByDepartment(Department department);
 
-
-
-     boolean existsByEmail(String email);
-
-     boolean existsById(@NonNull Long id);
+    boolean existsById(@NonNull Long id);
 }
---
+++ user-service/src/main/java/com/banka1/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.banka1.user.repository;
+
+import com.banka1.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
+
+	Optional<T> findByEmail(String email);
+
+	boolean existsByEmail(String email);
+}
