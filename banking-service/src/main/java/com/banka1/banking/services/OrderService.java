091e87c50148634f569e83e52746b5c0a1a1bb77
David Djuretanovic <djues3@gmail.com>
2025-04-18 06:22:09 +0200

    Fix merge conflicts
    Signed-off-by: David Djuretanovic <djues3@gmail.com>

--- banking-service/src/main/java/com/banka1/banking/services/OrderService.java
+++ banking-service/src/main/java/com/banka1/banking/services/OrderService.java
@@ -3,9 +3,7 @@ package com.banka1.banking.services;
 import com.banka1.banking.dto.MoneyTransferDTO;
 import com.banka1.banking.models.Account;
 import com.banka1.banking.repository.AccountRepository;
-import com.banka1.banking.services.implementation.AuthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
